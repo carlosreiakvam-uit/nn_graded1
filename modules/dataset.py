a75e7b40deffec984d364184dab0a740bf7a273a
carlos reiakvam <cre032@uit.no>
2022-10-04 10:51:31 +0200

    quick commments update

--- modules/dataset.py
+++ modules/dataset.py
@@ -12,22 +12,26 @@ import numpy as np
 
 class phosc_dataset(Dataset):
     def __init__(self, csvfile, root_dir, transform=None, calc_phosc=True):
-        self.transform = ""
-        self.root_dir = ""
 
+        # TODO: fill inn Dummy variables below
+        self.root_dir = ""
         self.df_all = pd.DataFrame(columns=["Image", "Word", "phos", "phoc", "phosc"])
         filename = ""
         word_label = ""
         phos = ""
         phosc = ""
         phosc = ""
+
+        # dr.ram
         # Fill in your code here. You will populate self.df_all
         # it should be pandas df with ["Image", "Word", "phos", "phoc", "phosc"] columns
         # containing file name, word label, phoc, phoc, phosc features vector in each row
         # phosc features vector can be created combining generate_phos_vector, generate_phoc_vector
         # Note: How to use phoc, phoc or phosc of the df in a batch is up to you.
         # in the __getitem__ below the phosc vector is used in the batches.
-        pass
+
+    def transform(self, image):
+        return image
 
     def __getitem__(self, index):
         img_path = os.path.join(self.root_dir, self.df_all.iloc[index, 0])
